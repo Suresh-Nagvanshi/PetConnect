321d276946a2f2f67842fa42447a2bff1bd22c93
Suresh-Nagvanshi <iamsureshnagvanshi@gmail.com>
2025-09-28 11:05:52 +0530

    add to cart done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,9 @@ import './styles/index.css';
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+ 
 )
